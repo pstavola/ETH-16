3611b50cbe8981be8e0015a6de76e2a580cb6e4c
Patrizio Stavola <8236762+pstavola@users.noreply.github.com>
2017-10-25 12:04:36 +0200

    Add files via upload
    SPLITTER
    
    Module 4 - Small Project
    
    You will create a smart contract named Splitter whereby:
    
    there are 3 people: Alice, Bob and Carol
    we can see the balance of the Splitter contract on the web page
    whenever Alice sends ether to the contract, half of it goes to Bob and the other half to Carol
    we can see the balances of Alice, Bob and Carol on the web page
    we can send ether to it from the web page
    It would be even better if you could team up with different people impersonating Alice, Bob and Carol, all cooperating on a test net.
    
    Stretch goals:
    
    add a kill switch to the whole contract
    make the contract a utility that can be used by David, Emma and anybody with an address
    cover potentially bad input data

---
+++ Splitter/contracts/Splitter.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.4.10;
+
+contract Splitter{
+    address public owner;
+    mapping(address=>uint) public balances;
+    bool active;
+
+    event LogSplittedValue(address receiver1, uint splittedValue1,address receiver2, uint splittedValue2, address sender, uint change);
+    event LogWithdraw(address requester, uint sendAmount);
+
+    modifier isActive()
+    {
+      require(active==true);
+      _;
+    }
+
+    modifier isOwner()
+    {
+      require(msg.sender == owner);
+      _;
+    }
+
+    function Splitter()
+      public payable
+    {
+      owner = msg.sender;
+      active = true;
+    }
+
+    function split(address receiver1, address receiver2)
+      public payable
+      isActive()
+      returns (bool success)
+    {
+      if(msg.value==0) revert();
+
+      balances[receiver1] += msg.value/2;
+      balances[receiver2] += msg.value/2;
+
+	  if (msg.value%2 > 0)
+		balances[msg.sender] += msg.value%2;
+
+      LogSplittedValue(receiver1, msg.value/2, receiver2, msg.value/2, msg.sender, msg.value%2);
+
+      success = true;
+    }
+
+    function getBalance()
+      constant
+      returns (uint)
+    {
+      return balances[msg.sender];
+    }
+
+    function withdrawFunds()
+      public
+      isActive()
+      returns (bool success)
+    {
+      require(balances[msg.sender]>0);
+
+      uint sendAmount = balances[msg.sender];
+      balances[msg.sender]=0;
+      msg.sender.transfer(sendAmount);
+
+      LogWithdraw(msg.sender, sendAmount);
+      success = true;
+    }
+
+    function stopSplitter()
+      public
+      isOwner()
+      returns (bool)
+    {
+      active=false;
+      return true;
+    }
+
+    function resumeSplitter()
+      public
+      isOwner()
+      returns (bool)
+    {
+      active=true;
+      return true;
+    }
+
+    function () public payable {}
+}
---
+++ Splitter/test/splitter.js
@@ -0,0 +1,109 @@
+var Splitter = artifacts.require("./Splitter.sol");
+
+contract('Splitter', function(accounts) {
+
+  var contract;
+
+  var owner = accounts[0];
+  var alice = accounts[1];
+  var bob = accounts[2];
+  var carol = accounts[3];
+
+  var evenAmount = 10;
+  var oddAmount = 5;
+  var oddAmountChange = oddAmount%2;
+  var oddAmountSplitted = (oddAmount-oddAmountChange)/2;
+
+
+  beforeEach(function(){
+    return Splitter.new({from: owner})
+    .then(function(instance){
+      contract = instance;
+    });
+  });
+
+  it("should split [even]", function() {
+    return contract.split(bob, carol, ({from:alice, value:evenAmount}))
+    .then(function(_success) {
+      success = _success;
+      assert.isOk(success, "Something went wrong");
+    })
+    .then(function(getBalance1) {
+      return contract.getBalance({from:bob})
+      .then(function(_balance1)
+      {
+        balance1 = _balance1.toString(10);
+        assert.equal(balance1,evenAmount/2,"Bob's balance is not correct");
+      })
+    })
+    .then(function(getBalance2) {
+      return contract.getBalance({from:carol})
+      .then(function(_balance2)
+      {
+        balance2 = _balance2;
+        assert.equal(balance2,evenAmount/2,"Carol's balance is not correct");
+      })
+    })
+  });
+  it("should split [odd]", function() {
+    return contract.split(bob, carol, ({from:alice, value:oddAmount}))
+    .then(function(_success) {
+      success = _success;
+      assert.isOk(success, "Something went wrong");
+    })
+    .then(function(getBalance1) {
+      return contract.getBalance({from:bob})
+      .then(function(_balance1)
+      {
+        balance1 = _balance1.toString(10);
+        assert.equal(balance1,oddAmountSplitted,"Bob's balance is not correct");
+      })
+    })
+    .then(function(getBalance2) {
+      return contract.getBalance({from:carol})
+      .then(function(_balance2)
+      {
+        balance2 = _balance2;
+        assert.equal(balance2,oddAmountSplitted,"Carol's balance is not correct");
+      })
+    })
+    .then(function(getBalanceSender) {
+      return contract.getBalance({from:alice});
+    }).then(function(_senderBalance){
+      senderBalance = _senderBalance;
+      assert.equal(senderBalance,oddAmount%2,"Alice's balance is not correct");
+    });
+  });
+  it("should withdraw requester's funds", function() {
+    return contract.split(bob, carol, ({from:alice, value:oddAmount}))
+    .then(function(_success) {
+      success = _success;
+      assert.isOk(success, "Something went wrong");
+    })
+    .then(function(withdrawFunds) {
+      return contract.withdrawFunds({from:bob})
+      .then(function(_success) {
+        success = _success;
+        assert.isOk(success, "Withdraw went wrong");
+      })
+      .then(function(getBalance1) {
+        return contract.getBalance({from:bob})
+        .then(function(_balance1)
+        {
+          balance1 = _balance1.toString(10);
+          assert.equal(balance1,0,"Bob's contract balance is not correct");
+        })
+      })
+    })
+	.then(function(txn){
+		for (var i = 0; i < txn.logs.length; i++) {
+			var log = txn.logs[i];
+			
+			if (log.event == "LogWithdraw") {
+				assert.equal(log.args[0],bob,"Bob is not the recipient");
+				assert.equal(log.args[1],oddAmount%2,"Value sent is not correct");
+			}
+		}
+	})
+  })
+});
