32725d0fed5afb93f18ee8a33948654b693d8633
Patrizio Stavola <patrizio.stavola@gmail.com>
2017-11-20 02:04:08 +0100

    added linked library to migration file

--- Remittance/migrations/2_deploy_contracts.js
+++ Remittance/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Remittance = artifacts.require("./Remittance.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, Remittance);
   deployer.deploy(Remittance);
 };
