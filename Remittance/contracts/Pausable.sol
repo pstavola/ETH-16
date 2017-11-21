b3daec00137f1d6a2ad92b4da5fe9f0d5f010579
Patrizio Stavola <patrizio.stavola@gmail.com>
2017-11-21 01:25:55 +0100

    changes to contracts files following @xavierlepretre comments

--- Remittance/contracts/Pausable.sol
+++ Remittance/contracts/Pausable.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.10;
 import "./Owned.sol";
 
 contract Pausable is Owned {
-    bool active;
+    bool private active;
 
-    event LogPauseStatusChange(address who, bool activeStatus);
+    event LogPauseStatusChange(address indexed who, bool activeStatus);
 
     modifier isActive{
         require(active);
@@ -30,6 +30,14 @@ contract Pausable is Owned {
         active = true;
         LogPauseStatusChange(msg.sender, active);
         return true;
+    }
+
+    function getStatus()
+        public
+        constant
+        returns (bool _active)
+    {
+		_active = active;
     }
 
     function () {
--- Remittance/contracts/Remittance.sol
+++ Remittance/contracts/Remittance.sol
@@ -6,13 +6,15 @@ import "./SafeMath.sol";
 contract Remittance is Pausable {
     using SafeMath for uint;
 
-    uint public fee;
-    uint public commissionsAmount;
+    uint private fee;
+    uint private commissionsAmount;
+	uint constant DURATION_LIMIT = 500;
 
-    event LogFeeSet(address who, uint fee);
-    event LogSendFunds(address sender, uint amout, bytes32 hashKey, uint duration);
-    event LogReleaseFunds(address exchange, uint sentAmount, uint fee);
-    event LogClaimBack(address claimer, uint sentAmount, uint blockNumber);
+    event LogFeeSet(address indexed who, uint fee);
+    event LogSendFunds(address indexed sender, uint amount, bytes32 hashKey, uint duration);
+    event LogReleaseFunds(address indexed exchange, uint sentAmount, uint fee);
+    event LogClaimBack(address indexed claimer, uint sentAmount);
+	event LogCommissionsWithdrawal(uint amountWithdrawn);
 
     struct Ledger {
       address sender;
@@ -26,25 +28,52 @@ contract Remittance is Pausable {
         public isOwner()
         returns (bool success)
     {
+		require(fee != _fee);
         fee = _fee;
         LogFeeSet(msg.sender, fee);
         success = true;
+    }
+
+    function getFee()
+        public
+        constant
+        returns (uint _fee)
+    {
+		_fee = fee;
+    }
+
+    function getCommissions()
+        public
+        constant
+        returns (uint _commissions)
+    {
+		_commissions = commissionsAmount;
+    }
+
+    function getDurationLimit()
+        public
+        constant
+        returns (uint _limit)
+    {
+		_limit = DURATION_LIMIT;
     }
 
     function sendFunds(bytes32 hashedKey, uint duration)
         public payable isActive()
         returns (bool success)
     {
-        require(duration<500);
+        require(duration<DURATION_LIMIT);
         require(msg.value>fee);
+        require(exchangeLedger[hashedKey].deadline==0);
+		
+		uint _deadline = SafeMath.add(block.number, duration);
 
-        uint _deadline = exchangeLedger[hashedKey].deadline;
-
-        require(_deadline==0);
-
-        exchangeLedger[hashedKey].sender = msg.sender;
-        exchangeLedger[hashedKey].amount = msg.value;
-        exchangeLedger[hashedKey].deadline = block.number + duration;
+        exchangeLedger[hashedKey] = Ledger({
+			sender : msg.sender,
+			amount : msg.value,
+			deadline : _deadline
+		});
+		
         LogSendFunds(msg.sender, msg.value, hashedKey, _deadline);
         success = true;
     }
@@ -57,7 +86,7 @@ contract Remittance is Pausable {
         require(exchangeLedger[hashedKey].amount>0);
         require(block.number<=exchangeLedger[hashedKey].deadline);
 
-        uint amountToSend = exchangeLedger[hashedKey].amount-fee;
+        uint amountToSend = SafeMath.sub(exchangeLedger[hashedKey].amount, fee);
         SafeMath.add(commissionsAmount, fee);
 
         exchangeLedger[hashedKey].amount = 0;
@@ -76,16 +105,16 @@ contract Remittance is Pausable {
         uint amountToSend = exchangeLedger[hashedKey].amount;
         exchangeLedger[hashedKey].amount = 0;
         msg.sender.transfer(amountToSend);
-        LogClaimBack(msg.sender, amountToSend, block.number);
+        LogClaimBack(msg.sender, amountToSend);
         success = true;
     }
 
-    function generateHash(address _v1, bytes32 _v2, bytes32 _v3)
+    function generateHash(address _exchange, bytes32 _pwdExchange, bytes32 _pwdReceiver)
         public
         constant
         returns (bytes32 pwdHash)
     {
-        pwdHash = keccak256(_v1, _v2, _v3);
+        pwdHash = keccak256(_exchange, _pwdExchange, _pwdReceiver);
     }
 
     function withdrawCommissions()
@@ -99,6 +128,8 @@ contract Remittance is Pausable {
 
         commissionsAmount = 0;
         msg.sender.transfer(toSend);
+		
+		LogCommissionsWithdrawal(toSend);
         success = true;
     }
 
