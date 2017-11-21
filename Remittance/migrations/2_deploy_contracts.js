3bc3920af3c7b2dd8ecccca414f5674f4de3f574
Patrizio Stavola <patrizio.stavola@gmail.com>
2017-11-21 01:34:52 +0100

    rolled back previous changes

--- Remittance/migrations/2_deploy_contracts.js
+++ Remittance/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 var Remittance = artifacts.require("./Remittance.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SafeMath);
-  deployer.link(SafeMath, Remittance);
   deployer.deploy(Remittance);
 };
